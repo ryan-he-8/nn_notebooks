b7e156ce045ec1c005ba08757e29c4f5c37146d4
Ryan He <ryanhxhe@gmail.com>
2025-12-20 22:09:48 -0800

    added bigrams to makemore

---
+++ makemore/bigram.py
@@ -0,0 +1,42 @@
+from torch import nn
+import torch
+
+class Bigram(nn.Module):
+
+    def __init__(self, dataset):
+        '''
+        dataset - list of names
+        '''
+        super().__init__() 
+
+        self.vocab = set()
+        for name in dataset:
+            for ch in name:
+                self.vocab.add(ch)
+
+        self.logits = torch.zeros((len(self.vocab)+1, len(self.vocab)+1))
+        self.logits += 1
+
+        sorted_vocab = ['.'] + sorted(list(self.vocab))
+        self.stoi = {c: i for i, c in enumerate(sorted_vocab)}
+        self.itos = {i: c for i, c in enumerate(sorted_vocab)}
+
+        for name in dataset:
+            new_name = '.' + name + '.'
+            for i, j in zip(new_name, new_name[1:]):
+                x1 = self.stoi[i]
+                x2 = self.stoi[j]
+                self.logits[x1][x2] += 1
+        
+        self.logits /= torch.sum(self.logits, dim=1, keepdim=True)
+
+    def forward(self, x, y=None):
+        
+        out = torch.multinomial(self.logits[x], num_samples=1, replacement=True)
+
+        loss = None
+
+        if y is not None:
+            loss = -torch.log(self.logits[x][out])
+
+        return out, loss
